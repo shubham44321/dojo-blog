c845eed86feba7d3590af59bea5605090694461a
shubham44321 <shubhampawar42@gmail.com>
2021-06-09 00:03:05 +0530

    Added new files

---
+++ src/BlogDetails.js
@@ -0,0 +1,35 @@
+import { useHistory, useParams } from "react-router-dom";
+import useFetch  from "./useFetch";
+
+const BlogDetails = () => {
+
+    const {id} = useParams();
+    const {data:blog,isPending,error} =  useFetch('http://localhost:8000/blogs/'+ id);
+    const history = useHistory();
+
+    const handleDelete = (e) => {
+        fetch('http://localhost:8000/blogs/'+blog.id,{
+            method:'DELETE'
+        })
+        .then(() => {
+            history.push('/')
+        })
+    }
+
+    return (  
+        <div className="blog-details">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                    <button onClick={handleDelete}>delete</button>
+                </article>
+            )}  
+        </div>
+    );
+}
+ 
+export default BlogDetails;
